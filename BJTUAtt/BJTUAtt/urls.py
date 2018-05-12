2cf0d7f47916b516069c219c5fc2799396c3175d
sunhanwu <1965190613@qq.com>
2018-05-12 13:35:12 +0800

    创建BJTUAtt django项目

--- BJTUAtt/BJTUAtt/urls.py
+++ BJTUAtt/BJTUAtt/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
- 
-from . import view
- 
+from django.contrib import admin
+from django.urls import path
+
 urlpatterns = [
-    url(r'^$', view.hello),
-    url(r'^plot/',view.plot),
+    path('admin/', admin.site.urls),
 ]
